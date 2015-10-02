c29b6442a147e829218bcac3f95cd7f190ed98f9
Jordan McCarthy <mccarthy@opentechinstitute.org>
2015-10-01 22:48:55 -0400

    Added documentation, cleaned up formatting and dead code, changed some silly variable names

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # ndt-probe-scheduler
+
+As currently implemented, the NDT Probe Scheduler is a generic python script 
+that is intended to serve as the scheduling "governor" for an arbitrary 
+number of satellite devices.  The IP addresses of the satellite devices
+must be statically assigned, and recorded in the in the [Probes]->addresses
+section of governer.cfg.  
+
+The other fields in governor.cfg specify the time windows within which tests 
+should be run, how many tests should be run (per device) per time window, and 
+what the minimum period of time between one test run and the next can be.  
+
+The script uses these parameters to calculate semi-random run times (that meet
+all specified parameters) at the start of each time window, and loads them 
+into a python scheduler structure.  Whenever any particular run time is reached,
+the governor directs the appropriate probe to initiate a test over ssh.  
+
+For example, the configuration shown in the default governor.cfg will result in
+3 tests being run from each of 0.0.0.0, 1.1.1.1, and 2.2.2.2, each at least 2
+minutes apart from the tests before and afer it (for a total of 9 tests, 
+requiring at least 27 minutes).  
+
+Design Notes
+
+In a server/client model, we should have each client exclusively pull config
+info from the server (so that the probes can be set up on private IPs). 
+The only information that needs to be sent to the server by any given probe is 
+the probe's unique ID. The only info that needs to be sent back to each probe 
+is the time of the probe's next test run. The client code should wait until 
+that time occurs, run a test, and then immediately contact the server to 
+request the next test run time.  A very handy extension would be for the 
+server to have some sort of alerting mechanism for sending an email if 3 
+consecutive run times elapse without a given probe having contacted the server
+
+The Probes section of the governor config file will have to be replaced with a 
+list of unique client IDs, which will also need to be manually assigned to each
+probe in client-side config files. There should be a separate server-side 
+config file for each school. All of them can be handled by the same server 
+process, which will load all .cfg files in particular directory whenever
+it launches.  
+
+The client-side config files should only need unique IDs, and the URL of the 
+governor server.  
+
+The client process should periodically run some sort of NTP check to make sure
+that its local clock is correct. The client process should also be responsible
+for running get\_ndt\_server, so that the result returned by mlab-ns reflects
+the location of the probes, not the governor server.  
+
+The client process should be protected by some sort of wrapper or status 
+checker -- perhaps a script in /etc/cron.hourly that makes sure that the client
+process is running, and automatically starts it if it isn't.  
+
--- governor.py
+++ governor.py
@@ -6,6 +6,7 @@ last_run=0
 
 governor = sched.scheduler(time.time, time.sleep)
 
+   #This function should always be run from the network on which the probes are located.  
 def get_ndt_server():
    mlabns=urllib2.urlopen('http://mlab-ns.appspot.com/ndt')
    server = dict(json.loads(mlabns.read()))['fqdn'].encode('ascii')
@@ -15,21 +16,22 @@ def run_ndt (addr):
    ndt_server = get_ndt_server()
    ndt_cmd="web100clt -n " + ndt_server + " --disablesfw --disablemid"
    print "Running test on device " + addr + " at " + time.strftime("%x,%H:%M:%S")
-   test_output = subprocess.Popen(["ssh", addr, ndt_cmd],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+   test_output = subprocess.Popen(["ssh", addr, ndt_cmd],stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
    log_text = test_output.communicate()
-   thingy = open("beta.log", 'a')
-   thingy.write(time.strftime("\n-------\n%x,%H:%M:%S\n" + ndt_server + "\n"))
+   logfile = open(addr + ".log", 'a')
+   logfile.write(time.strftime("\n-------\n%x,%H:%M:%S\n" + ndt_server + "\n"))
    for line in log_text[0].split('\n'):
       print line
-      thingy.write(line + "\n")
-   thingy.close()
-   #This will only work if the SSH infrastucture on the governor and the probes has been set up correctly
-
-
-#def parse_timing():
+      logfile.write(line + "\n")
+   logfile.close()
+   # This will only work if the SSH infrastucture on the governor and the probes
+   # has been set up correctly. If/when this code is restructed into server and 
+   # client components, this code will have to be largely scrapped
 
 
 while True:
+   #Initialize time, and read config file
    now = time.localtime()
    if last_run < now:
       last_run = now
@@ -37,20 +39,26 @@ while True:
    probes_to_run = config.get("Probes", "addresses").split()
    interval = int(config.get("Timing", "delay"))*60
    runs_per_period = int(config.get("Timing", "runs_per_period"))
-   #Assemble the list of today's period boundaries by mashing together today's date and the generic hour:minute entries from the config file
+
+   # Assemble the list of today's period boundaries by mashing together today's 
+   # date and the generic hour:minute entries from the config file
    period_boundaries = [time.strptime(time.strftime("%x", now) + "," + t, "%x,%H%M") for t in config.get("Timing", "boundaries").split()]
-   #Identify the next period boundary
+
+   #Identify the next (upcoming) period boundary
    for next_period in period_boundaries:
       if next_period > now:
          if last_run < next_period: #WRONG.  Also, you need to add the frequency
 #            print str(last_run) + ", " + str(next_period)
             break
+
    #If we get to the end of the list of period boundaries and nothing is in the future, roll over to the next day, and use the first period boundary in the config file
    #TODO: This logic will break at the day boundary at the end of a leapyear, when %j will be incremented to 367, which is out of the range "time" accepts 
    else:
       next_period = time.strptime(str(now.tm_year) + "," + str(now.tm_yday+1) + "," + config.get("Timing", "boundaries").split().pop(0), "%Y,%j,%H%M")
-      #TODO:Explicitly sort the boundary list before blindly grabbing the first element?
+      #TODO:Should we sort the boundary list before blindly grabbing the first element?
 
+   #Sanity check to make sure that the parameters specified in the config file are possible to implement in the current time window
+   #TODO: This will break if the governor is launched shortly befor the next time boundary.  Perhaps just skip to the next time boundary in that case, and bail only if the parameters also prove impossible to implement in that next window?
    if time.mktime(now) + runs_per_period*(test_duration+((len(probes_to_run)-1)*(test_duration+interval))) > time.mktime(next_period):
       raise RuntimeError("Specified test delay is too long, and/or specified periods are too short, to accomidate the number of devices.")
    else:
@@ -78,13 +86,3 @@ while True:
 #      counter=counter+1
    last_run=next_period
    governor.run()   
-
-
-
-
-
-#load buckets
-#assume that current bucket has not been run yet
-#schedule using random(now, bucket boundary)
-#sleep
-#Wake up, run test, finish loop.  
